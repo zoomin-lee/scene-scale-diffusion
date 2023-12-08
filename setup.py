9a7e14ac5e9f49cda1507dd1d9dc02797082945b
zoomin-lee <65997635+zoomin-lee@users.noreply.github.com>
2023-12-08 21:32:07 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scene_scale_diffusion",
+    version="0.1",
+    author="Lee Jumin, Im Woobin, Lee Sebin, Yoon Sung-Eui",
+    author_email="",
+    description="Experiments in PyTorch",
+    long_description="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
